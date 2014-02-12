731751a821c95fb9ece93a6bcfcf4af12d74a3ba
Kenneth Cachia <kcachia@gmail.com>
2014-02-12 07:31:04 -0500

    Call action() and fire action event on menu click.

--- ui/menu-item.js
+++ ui/menu-item.js
@@ -2,13 +2,32 @@
 /*
  * Menu Item
  */
-var MenuItem = Create({
+var MenuItem = Create('MenuItem', {
+
+  _onClick: function () {
+    var action = this.get('action');
+
+    if (action) {
+      action();
+    }
+
+    this.fire('action');
+  },
+
 
   _attrs: {
+    action: null,
     label: null,
+    mergeData: ['label'],
+
     container: '<div class="menu-item"></div>',
     template: '<div class="menu-item-title">{{label}}</div>',
-    mergeData: ['label']
+
+    domEvents: [{
+      selector: '.menu-item',
+      eventName: 'click',
+      callback: '_onClick'
+    }]
   }
 
 }, View);
