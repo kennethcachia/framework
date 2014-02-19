b57a2837e4975bcafefb4ea6a4954a75908ea73b
Kenneth Cachia <kcachia@gmail.com>
2014-02-19 09:05:03 -0500

    Basic SVG modules.

---
+++ scripts/svg/svg.js
@@ -0,0 +1,39 @@
+
+var SVGElement = Create('SVGElement', {
+
+  _attrs: {
+    namespaceURI: 'http://www.w3.org/2000/svg'
+  }
+
+}, NodeElement);
+
+
+var SVG = Create('SVG', {
+
+  _attrs: {
+    html: '<svg></svg>'
+  }
+
+}, SVGElement);
+
+
+var SVGRect = Create('SVGRect', {
+
+  _attrs: {
+    html: '<rect x="10" y="10" width="100" height="100"/>'
+  }
+
+}, SVGElement);
+
+
+var SVGCanvas = Create('SVG', {
+
+  addShape: function (shape) {
+    this.get('container').appendChild(shape);
+  },
+
+  _attrs: {
+    container: new SVG()
+  }
+
+}, View);
