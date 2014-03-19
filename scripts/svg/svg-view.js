2eb88064458d609c4954e1d1cf9a3834a0f41e8a
Kenneth Cachia <kcachia@gmail.com>
2014-03-19 08:29:32 -0400

    SVGView - add a class to child shapes.

--- scripts/svg/svg-view.js
+++ scripts/svg/svg-view.js
@@ -7,12 +7,16 @@ define(['core/create', 'svg/svg', 'mv/view'], function (Create, SVG, View) {
   var SVGView = Create('SVGView', {
 
     addShape: function (shape) {
+      var shapeClass = this.get('shapeClass');
+      shape.addClass(shapeClass);
+
       this.get('container').appendChild(shape);
     },
 
 
     _attrs: {
-      container: new SVG()
+      container: new SVG(),
+      shapeClass: 'svg-shape'
     }
 
   }, View);
