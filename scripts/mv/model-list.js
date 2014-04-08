4db82ba321f37c8099dce80fa5d3cb838f4c7726
Kenneth Cachia <kcachia@gmail.com>
2014-04-08 16:43:25 -0400

    ModelList - cleanup.

--- scripts/mv/model-list.js
+++ scripts/mv/model-list.js
@@ -1,52 +1,29 @@
 
-define(['core/Create', 'mv/model'], function (Create, Model) {
+define([
+
+  'core/Create', 'mv/model'
+
+], function (Create, Model) {
 
   /**
    * ModelList
    */
   var ModelList = Create('ModelList', {
 
-    initializer: function () {
-      var Type = this.get('type');
-      var json = this.get('json');
-
-      this._data = [];
-
-      // TODO: custom type defined in json -- makes sense?
-      json.each(json, function (attr) {
-        this._data.push(new Type(attr));
-      }, this);
-    },
-
-
     getData: function () {
-      var data = [];
+      var items = this.get('items');
 
-      // TODO: use List
-      for (var d = 0; d < this._data.length; d++) {
-        data.push(this._data[d].getData());
-      }
-
-      return data;
-    },
-
-
-    add: function (json) {
-      // Add from raw data
-    },
-
-
-    remove: function (id) {
-      // Remove by id
+      return {
+        data: items
+      };
     },
 
 
     _attrs: {
-      type: Model,
-      json: []
+      items: []
     }
 
-  });
+  }, Model);
 
 
   return ModelList;
