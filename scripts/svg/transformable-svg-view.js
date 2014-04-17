2da16127b3dbcc11087e93f5e6f21f2aa19758e1
Kenneth Cachia <kcachia@gmail.com>
2014-04-16 23:40:42 -0400

    TransformableSVGView.

---
+++ scripts/svg/transformable-svg-view.js
@@ -0,0 +1,105 @@
+
+define([
+
+  'core/create',
+  'svg/svg-view',
+  'core/dom-element'
+
+], function (Create, SVGView, DOMElement) {
+
+  /**
+   * SVG View
+   */
+  var TransformableSVGView = Create('TransformableSVGView', {
+
+    initializer: function () {
+      this._bindEvents();
+    },
+
+
+    destructor: function () {
+      for (var e = 0; e < this._events.length; e++) {
+        this._events[e].destroy();
+      }
+    },
+
+
+    _bindEvents: function () {
+      var targets = this.get('selector');
+      var container = this.get('container');
+
+      this._events = [];
+
+      this._events.push(container.addDOMEvent({
+        eventName: 'mousedown',
+        callback: '_onTargetMouseDown',
+        context: this
+      }));
+
+      var body = new DOMElement();
+
+      body.fromNode(document.body);
+
+      this._events.push(body.addDOMEvent({
+        eventName: 'mouseup',
+        callback: '_onTargetMouseUp',
+        context: this
+      }));
+
+      this._events.push(container.addDOMEvent({
+        eventName: 'mousemove',
+        callback: '_onTargetMouseMove',
+        context: this
+      }));
+    },
+
+
+    _onTargetMouseDown: function (domElement, e) {
+      var children = this.getRenderedChildren();
+
+      var child;
+      var childContainer;
+
+      var x;
+      var y;
+
+      for (var c = 0; c < children.length; c++) {
+        child = children[c];
+        childContainer = child.get('container');
+
+        if (childContainer.isEqualTo(domElement)) {
+          x = child.get('data').x;
+          y = child.get('data').y;
+
+          console.log('down');
+          this._activeView = child;
+          break;
+        }
+      }
+    },
+
+
+    _onTargetMouseUp: function () {
+      this._activeView = null;
+      console.log('up');
+    },
+
+
+    _onTargetMouseMove: function (domElement, e) {
+      console.log('move');
+
+      if (this._activeView) {
+        var x = e.layerX;
+        var y = e.layerY;
+
+        this._activeView.set('data.x', x);
+        this._activeView.set('data.y', y);
+      }
+    }
+
+  }, SVGView);
+
+
+  return TransformableSVGView;
+
+});
