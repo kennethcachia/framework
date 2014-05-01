7c8ad4932ba75b13f84a328cf80b2328def1ad48
Kenneth Cachia <kcachia@gmail.com>
2014-04-30 21:33:40 -0400

    GridView - add items attribute.

--- scripts/ui/grid-view.js
+++ scripts/ui/grid-view.js
@@ -11,7 +11,25 @@ define([
    */
   var ViewGrid = Create('ViewGrid', {
 
+    initializer: function () {
+      this.on('data.itemsChange', this._onItemsChange, this);
+    },
+
+
+    _onItemsChange: function () {
+      var items = this.get('data').items;
+
+      this.set('children', items);
+    },
+
+
     _attrs: {
+      data: {
+        value: {
+          items: []
+        }
+      },
+
       container: {
         value: '<div class="ui-grid"></div>'
       }
