d51ee7fb87ab5b9ba5be59f8d6f6cf072702a108
Kenneth Cachia <kcachia@gmail.com>
2014-05-01 22:22:57 -0400

    Controller - render view after initReady.

--- scripts/mv/controller.js
+++ scripts/mv/controller.js
@@ -15,6 +15,8 @@ define([
 
       view.on('appendedView', this._updateModel, this);
       view.on('childDataChange', this._updateModel, this);
+
+      this.on('initReady', this._render, this);
     },
 
 
@@ -27,14 +29,19 @@ define([
     },
 
 
-    _updateModel: function (e) {
-      this.fire('updateModel', e);
+    // It's important that the view is rendered
+    // after all the initializers are called,
+    // as it's common to bind events on the view
+    // in the controller's initializer() that might
+    // initially fire after the view is rendered.
+    _render: function () {
+      var view = this.get('view');
+      view.render();  
     },
 
 
-    render: function () {
-      var view = this.get('view');
-      view.render();
+    _updateModel: function (e) {
+      this.fire('updateModel', e);
     },
 
 
