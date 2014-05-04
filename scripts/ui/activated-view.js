06cd413ec50a5b6b32ce84508c54114684de1865
Kenneth Cachia <kcachia@gmail.com>
2014-05-04 12:08:48 -0400

    ActivatedView - a view with an active state.

---
+++ scripts/ui/activated-view.js
@@ -0,0 +1,47 @@
+
+define([
+
+  'core/create',
+  'mv/view'
+
+], function (Create, View) {
+
+  /**
+   * ActivatedView
+   */
+  var ActivatedView = Create('ActivatedView', {
+
+    activate: function () {
+      this._setActive(true);
+    },
+
+
+    deactivate: function () {
+      this._setActive(false);
+    },
+
+
+    _setActive: function (isActive) {
+      var container = this.get('container');
+      var activeClassName = this.get('activeClassName');
+
+      if (isActive) {
+        container.addClass(activeClassName);
+      } else {
+        container.removeClass(activeClassName);
+      }
+    },
+
+
+    _attrs: {
+      activeClassName: {
+        value: 'activated-view--active'
+      }
+    }
+
+  }, View);
+
+
+  return ActivatedView;
+
+});
