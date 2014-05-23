77b6f23e9250e4763d52e9e0d13a7555b444d3ba
Kenneth Cachia <kcachia@gmail.com>
2014-05-22 21:06:58 -0400

    Attributes - cleanup.

--- scripts/core/attributes.js
+++ scripts/core/attributes.js
@@ -11,45 +11,18 @@ define([
   var Attributes = function (initAttrs, objAttrs) {
     var attrs = Utils.clone(initAttrs) || {};
 
-    // Set an id
-    attrs.id = attrs.id || null;
+    this._expandAttrs(attrs);
+    this._attrs = Utils.mergeObjects(attrs, objAttrs);
 
-    // Convert to value: {..}
-    for (var a in attrs) {
-      attrs[a] = {
-        value: attrs[a]
-      };
-    }
-
-    // Merge attributes
-    if (attrs) {
-      this._attrs = Utils.mergeObjects(attrs, objAttrs);
-    }
-
-
-    // Call default() if not defined
-    var attrValue;
-    var attrDefault;
-
-    for (var a in this._attrs) {
-      attrValue = this._attrs[a].value;
-      attrDefault = this._attrs[a].default;
-
-      if (attrDefault && (attrValue === null || attrValue === undefined)) {
-        this._attrs[a].value = this._attrs[a].default.call(this);
-      }
-    }
-
+    this._setDefaults();
   };
 
 
   Attributes.prototype = {
-
     set: function (key, value, context) {
       var attrs = this._attrs;
 
-      // Allow attributes to be 
-      // added dynamically
+      // Allow attributes to be added dynamically
       if (!attrs[key]) {
         attrs[key] = {};
       }
@@ -62,8 +35,8 @@ define([
     },
 
 
-    setObj: function (obj, index, value) {
-      this._attrs[obj].value[index] = value;
+    setObj: function (obj, key, value) {
+      this._attrs[obj].value[key] = value;
     },
 
 
@@ -81,10 +54,33 @@ define([
       }
 
       return val;
+    },
+
+
+    // Convert to value: {..}
+    _expandAttrs: function (attrs) {
+      for (var a in attrs) {
+        attrs[a] = {
+          value: attrs[a]
+        };
+      }
+    },
+
+
+    _setDefaults: function () {
+      var attrValue;
+      var attrDefault;
+
+      for (var a in this._attrs) {
+        attrValue = this._attrs[a].value;
+        attrDefault = this._attrs[a].default;
+
+        if (attrDefault && (attrValue === null || attrValue === undefined)) {
+          this._attrs[a].value = this._attrs[a].default.call(this);
+        }
+      }
     }
-
   };
-
 
 
   return Attributes;
