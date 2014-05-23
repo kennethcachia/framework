a1a51392ef3d5403abc790a46ec9cc85c25a3a8d
Kenneth Cachia <kcachia@gmail.com>
2014-05-22 20:48:30 -0400

    Create - cleanup.

--- scripts/core/create.js
+++ scripts/core/create.js
@@ -14,36 +14,46 @@ define([
       throw new Error('No Name');
     }
 
-    var _initializers = [];
-    var _destructors = [];
-    var _attrs = own._attrs || {};
+    var initializers = [];
+    var destructors = [];
+    var attrs = own._attrs || {};
 
     if (extension) {
-      _initializers = extension.prototype._initializers.slice(0);
-      _destructors = extension.prototype._destructors.slice(0);
-      _attrs = Utils.mergeObjects(_attrs, extension.prototype._attrs);
+      initializers = extension.prototype._initializers.slice(0);
+      destructors = extension.prototype._destructors.slice(0);
+      attrs = Utils.mergeObjects(attrs, extension.prototype._attrs);
     }
 
+    var BaseObject = createBaseObject();
 
-    var _BASE = function (attrs) {
+    Utils.extend(own, BaseObject.prototype, initializers, destructors);
+
+    if (extension) {
+      Utils.extend(extension.prototype, BaseObject.prototype);
+      BaseObject.super = extension.prototype;
+    }
+
+    BaseObject.prototype._initializers = initializers;
+    BaseObject.prototype._destructors = destructors;
+    BaseObject.prototype._attrs = attrs;
+    BaseObject.prototype._name = name;
+
+    return BaseObject;
+  }
+
+
+  function createConstructorFn() {
+    return function (attrs) {
       this.init(attrs);
     };
+  }
 
-    _BASE.prototype = Utils.clone(Base);
 
-    Utils.extend(own, _BASE.prototype, _initializers, _destructors);
+  function createBaseObject() {
+    var BaseObject = createConstructorFn();
+    BaseObject.prototype = Utils.clone(Base);
 
-    if (extension) {
-      Utils.extend(extension.prototype, _BASE.prototype);
-      _BASE.super = extension.prototype;
-    }
-
-    _BASE.prototype._initializers = _initializers;
-    _BASE.prototype._destructors = _destructors;
-    _BASE.prototype._attrs = _attrs;
-    _BASE.prototype._name = name;
-
-    return _BASE;
+    return BaseObject;
   }
 
 
