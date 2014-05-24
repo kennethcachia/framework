bb89d78bdb092f5b88ff69e59acb6eddd1556e8c
Kenneth Cachia <kcachia@gmail.com>
2014-05-24 14:04:04 -0400

    Model - cleanup.

--- scripts/mv/model.js
+++ scripts/mv/model.js
@@ -10,43 +10,12 @@ define([
    */
   var Model = Create('Model', {
 
-    initializer: function () {
-      var attrs = this._attrs;
+    parse: function (response) {
+      var json = JSON.parse(response);
 
-      for (var a in attrs) {
-        this.on(a + 'Change', this._updated, this);
-      }
-    },
-
-
-    _updated: function () {
-      this.fire('updated');
-    },
-
-
-    pushToKey: function (key, data) {
-      var items = this.get(key);
-
-      // TODO: throw error if not array
-      items.push(data);
-
-      this.set(key, items);
-    },
-
-
-    getData: function () {
-      var data = {};
-      var attrs = this._attrs;
-
-      for (var a in attrs) {
-        data[a] = attrs[a];
-      }
-
-      return data;
-    },
-
-
-    _attrs: { }
+      // Validate response against _attrs
+      this.setFromObject(json, false);
+    }
 
   });
 
